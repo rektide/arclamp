c33e409471ccdcf770861da95d41052614300f2c
rektide <rektide@voodoowarez.com>
2010-12-15 05:56:59 +0000

    migrate to jsdoc-based documentation.

--- src/arclamp.js
+++ src/arclamp.js
@@ -1,3 +1,10 @@
+/**
+ * @name arclamp
+ * @description xml to json converter, sax based.
+ * @returns JSON rasterized xml document
+ * @param doc input xml string to parse into json
+ * @author <a href="http://voodoowarez.com/rektide">rektide</a> &lt;<a href="mailto:rektide@voodoowarez.com">rektide@voodoowarez.com</a>&gt;
+ */
 var arclamp = (this.exports||(exports = {})).arclamp = function(doc) {
 	// insure context
 
@@ -5,38 +12,101 @@ var arclamp = (this.exports||(exports = {})).arclamp = function(doc) {
 		return new arclamp(doc)
 
 	// parsing members
-	
-	this.doc = doc // document to parse
-	this.defaultHandler2 = new DefaultHandler2() // sax handler
-	this.saxXmlReader = XMLReaderFactory.createXMLReader() // sax reader
-	this.resetBetweenParses = false // optional step to clean root
+
+	/**
+	  document to parse
+	  @field
+	  @private
+	*/
+	this.doc = doc
+	/**
+	  sax handler
+	  @field
+	  @private
+	*/
+	this.defaultHandler2 = new DefaultHandler2()
+	/**
+	  sax reader
+	  @field
+	  @private
+	*/
+	this.saxXmlReader = XMLReaderFactory.createXMLReader()
+	/**
+	  optional step to clean root between parses
+	  @field
+	  @private
+	*/
+	this.resetBetweenParses = false
+	/**
+	  logger
+	  @field
+	  @private
+        */
 	this.log = function(args,s,name) { console.log(args,s,name) }
 	//this.log = function(args,s,name) {}
-	
+
 	// context members
 
-	this.root = {} // arclamp builds this object
-	this.stack = [this.root] // element stack
-	this.defaultNamespaces = [] // prefix stack
-	this.prefixes = {} // named prefixes
+	/**
+	  arclamp builds this JavaScript object
+	  @field
+	*/
+	this.root = {}
+	/**
+	  element stack
+	  @field
+	  @private
+	*/
+	this.stack = [this.root]
+	/**
+	  default namespace stack
+	  @field
+	  @private
+	*/
+	this.defaultNamespaces = []
+	/**
+	  qualified prefixes
+	  @field
+	  @private
+	*/
+	this.prefixes = {}
 
 	var self = this
 
 	// utility functions
+	/**
+	  get the top element from an array
+	  @private
+	*/
 	var top = function(array) {
 		return array[array.length-1]
 	}
-	this.__proto__.topStack = function() { return top(self.stack) }
-	this.__proto__.topNamespace = function() { return top(self.defaultNamespaces) }
+	/**
+	  top of element stack
+	  @private
+	*/
+	this.topStack = function() { return top(self.stack) }
+	/**
+	  top of default namespace stack
+	*/
+	this.topNamespace = function() { return top(self.defaultNamespaces) }
 
 	// sax functions
 
+	/**
+	  capture prefixes in the prefix map, and the default namespaces if appropriate.
+	  @event
+	*/
 	this.__proto__.startPrefixMapping = function(prefix,uri) {
 		self.log(arguments,self,"startPrefixMapping")
 		self.prefixes[prefix] = uri
 		if(!prefix)
 			self.defaultNamespaces.push(uri)
 	}
+	/**
+	  pop the default namespaces stack if we're ending a default namespace.
+	  @event
+	*/
 	this.__proto__.endPrefixMapping = function(prefix) {
 		self.log(arguments,self,"endPrefixMapping")
 		if(prefix)
@@ -48,6 +118,10 @@ var arclamp = (this.exports||(exports = {})).arclamp = function(doc) {
 			self.prefixes[""] = self.topNamespace()
 		}
 	}
+	/**
+	  builds & captures a new element on it's parent & descend into by adding to the elements stack.
+	  @event
+	*/
 	this.__proto__.startElement = function(uri,localName,qName,attributes) {
 		self.log(arguments,self,"startElement")
 		// build element & attach to tree
@@ -71,10 +145,18 @@ var arclamp = (this.exports||(exports = {})).arclamp = function(doc) {
 		// descend into
 		self.stack.push(neo)
 	}
+	/**
+	  pop the element stack
+	  @event
+	*/
 	this.__proto__.endElement = function(uri,localName,qName) {
 		self.log(arguments,self,"endElement")
 		self.stack.pop()
 	}
+	/**
+	  read in some character values
+	  @event
+	*/
 	this.__proto__.characters = function(ch,start,length) {
 		self.log(arguments,self,"characters")
 		var cur = self.topStack()
@@ -88,9 +170,17 @@ var arclamp = (this.exports||(exports = {})).arclamp = function(doc) {
 	this.parse() // run sax parser & build root
 	return this.root
 }
+/**
+  parse the arclamp's document
+*/
 arclamp.prototype.parse = function() {
+	if(this.resetBetweenParses)
+		this.root = {}
 	this.saxXmlReader.parseString(this.doc)
 }
+/**
+  wire up arclamp's handlers
+*/
 arclamp.prototype.wire = function() {
 	this.defaultHandler2.endPrefixMapping = this.endPrefixMapping
 	this.defaultHandler2.startPrefixMapping = this.startPrefixMapping
